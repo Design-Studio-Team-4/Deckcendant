095689e803fbf7828f73bfee09e262bdeedbe37d
Darksparky <Darksparky@users.noreply.github.com>
2020-01-26 17:10:13 -0500

    Changed to fix asset issue not syncing

--- Deckcendant/Assets/Hand.cs
+++ Deckcendant/Assets/Hand.cs
@@ -1,18 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*
- HAND
-The Hand represents the cards that the player can choose from in the current turn. These cards are desplayed at the forefront of any combat encounter and the hand is the player's primary interface with the game. 
-Due to it being the primary interface a lot of care needs to be put into the tiny details of how the hand works. 
 
-TODO:
-- Divide Drawhand method into a method that takes in a GIVEN number of cards from a given pile to the card array(hand), a method that GIVES out a GIVEN number of cards to a chosen pile(passes the data on and destroys object), and 
-  a method that displays (instanciates) the current hand so that it places the hand nicely, accounting for the amount of cards there are and the alotted space for the hand to be displayed in.  
-- Create a method which takes checks for which card the mouse is over and brings it into full veiw and displays the discription.
-- Create a method that selects a card and targets an object (friend or foe); 
 
-*/
 public class Hand : MonoBehaviour
 {
 
@@ -35,7 +25,7 @@ public class Hand : MonoBehaviour
     }
     public void MoveCardsToHand(int numCrds, GameObject[] crds, GameObject sender)
     {
-
+         
     }
     public void MoveCardsFromHand(int numCrds, GameObject[] crds, GameObject destination)
     {
@@ -77,3 +67,15 @@ public class Hand : MonoBehaviour
         }
     }
 }
+/*
+ HAND
+The Hand represents the cards that the player can choose from in the current turn. These cards are desplayed at the forefront of any combat encounter and the hand is the player's primary interface with the game. 
+Due to it being the primary interface a lot of care needs to be put into the tiny details of how the hand works. 
+
+TODO:
+- Divide Drawhand method into a method that takes in a GIVEN number of cards from a given pile to the card array(hand), a method that GIVES out a GIVEN number of cards to a chosen pile(passes the data on and destroys object), and 
+  a method that displays (instanciates) the current hand so that it places the hand nicely, accounting for the amount of cards there are and the alotted space for the hand to be displayed in.  
+- Create a method which takes checks for which card the mouse is over and brings it into full veiw and displays the discription.
+- Create a method that selects a card and targets an object (friend or foe); 
+
+*/
