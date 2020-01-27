95193afd2574c1e0a4f8a912f8f6a819b30076a2
Darksparky <Darksparky@users.noreply.github.com>
2020-01-26 19:39:22 -0500

    Set up some more stuff will work on it more tomorrow

--- Deckcendant/Assets/BattleHandler.cs
+++ Deckcendant/Assets/BattleHandler.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class BattleHandler : MonoBehaviour
 {
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform enemy;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnCombatant(true, player);
+        SpawnCombatant(false, enemy);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void SpawnCombatant(bool isFriendly, Transform combatant)
+    {
+        Vector3 pos;
+       
+        if (isFriendly)
+        {
+            pos = new Vector3(-5.0f, 0.0f, 0.0f);
+
+        } else
+        {
+            pos = new Vector3(+5.0f, 0.0f, 0.0f);
+        }
+        Instantiate(combatant, pos, combatant.transform.rotation);
     }
 }
