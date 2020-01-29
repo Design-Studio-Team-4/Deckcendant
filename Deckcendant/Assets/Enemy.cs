0d40c2cb2338dd6a0f091d3357a6ad129eacbcd5
Yaseen Zahid <yaseen197@hotmail.com>
2020-01-29 12:02:07 -0500

    made enemy

---
+++ Deckcendant/Assets/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    int health;
+    int cooldown;
+
+    private static System.Random rand = new Random();
+
+    public void TakeDamage(Card c) { health -= c.efPower; }
+
+    public void ChooseAttack()
+    {
+        int i = rand.Next(/* NUMBER OF ATTACKS*/);
+
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
